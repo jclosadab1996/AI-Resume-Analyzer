21fce98ce71f53429d31ea106f00fbf58990663e
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2025-08-02 23:42:45 -0500

    feat: add ResumeCard component and implement resume display logic

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,5 @@
 import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -19,6 +20,16 @@ export default function Home() {
           <h2>Review Your Submissions And Check AI-Powered feedback.</h2>
         </div>
       </section>
+
+      {resumes.lenght > 0 && (
+      <div className="resumes-section">
+        {resumes.map( callbackfn:(resume) => (
+        <ResumeCard key={resume.id} resume={resume} />
+        ))}
+      </div>
+      )}
+
+      
     </main>
   );
 }
